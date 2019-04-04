8afca2ddea4f36358ab7b75bd82b146c94122961
Shawn M. Crawford <sleepy3d@gmail.com>
2019-04-04 14:18:28 -0400

    initial commit

---
+++ PopSMSPassGen/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace PopSMSPassGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxHealth = new System.Windows.Forms.ComboBox();
+            this.comboBoxRemainingTime = new System.Windows.Forms.ComboBox();
+            this.buttonGeneratePassword = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxRemainingTime);
+            this.groupBox1.Controls.Add(this.comboBoxHealth);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBoxLevel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 107);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // comboBoxLevel
+            // 
+            this.comboBoxLevel.FormattingEnabled = true;
+            this.comboBoxLevel.Location = new System.Drawing.Point(99, 19);
+            this.comboBoxLevel.Name = "comboBoxLevel";
+            this.comboBoxLevel.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLevel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Level:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Health:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Remaining Time:";
+            // 
+            // comboBoxHealth
+            // 
+            this.comboBoxHealth.FormattingEnabled = true;
+            this.comboBoxHealth.Location = new System.Drawing.Point(99, 46);
+            this.comboBoxHealth.Name = "comboBoxHealth";
+            this.comboBoxHealth.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxHealth.TabIndex = 5;
+            // 
+            // comboBoxRemainingTime
+            // 
+            this.comboBoxRemainingTime.FormattingEnabled = true;
+            this.comboBoxRemainingTime.Location = new System.Drawing.Point(99, 73);
+            this.comboBoxRemainingTime.Name = "comboBoxRemainingTime";
+            this.comboBoxRemainingTime.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxRemainingTime.TabIndex = 6;
+            // 
+            // buttonGeneratePassword
+            // 
+            this.buttonGeneratePassword.Location = new System.Drawing.Point(9, 17);
+            this.buttonGeneratePassword.Name = "buttonGeneratePassword";
+            this.buttonGeneratePassword.Size = new System.Drawing.Size(75, 23);
+            this.buttonGeneratePassword.TabIndex = 7;
+            this.buttonGeneratePassword.Text = "&Generate";
+            this.buttonGeneratePassword.UseVisualStyleBackColor = true;
+            this.buttonGeneratePassword.Click += new System.EventHandler(this.buttonGeneratePassword_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(99, 19);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(121, 20);
+            this.textBoxPassword.TabIndex = 8;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonGeneratePassword);
+            this.groupBox2.Controls.Add(this.textBoxPassword);
+            this.groupBox2.Location = new System.Drawing.Point(12, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 53);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PopSMSPassGen.Properties.Resources.popsms;
+            this.pictureBox1.Location = new System.Drawing.Point(258, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 188);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Prince of Persia Passgen (SMS) - sleepy9090";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxRemainingTime;
+        private System.Windows.Forms.ComboBox comboBoxHealth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxLevel;
+        private System.Windows.Forms.Button buttonGeneratePassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PopSMSPassGen/Form1.cs
@@ -0,0 +1,168 @@
+﻿/**
+ *  @file           Form1.cs / PopSMSPassGen
+ *  @brief          Password Generator for the Sega Master System Game Prince of Persia
+ *  @copyright      Shawn M. Crawford 2019
+ *  @date           04/04/2019
+ *
+ *  @remark Author  Shawn M. Crawford (sleepy9090)
+ *
+ *  @note           N/A
+ *
+ */
+using System;
+using System.Windows.Forms;
+
+namespace PopSMSPassGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            textBoxPassword.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            populateComboBoxes();
+        }
+
+        private void populateComboBoxes()
+        {
+            for (int x = 1; x <= 14; x++)
+            {
+                comboBoxLevel.Items.Add(x);
+            }
+            comboBoxLevel.SelectedIndex = 0;
+
+            for (int x = 0; x <= 7; x++)
+            {
+                comboBoxHealth.Items.Add(x);
+            }
+            comboBoxHealth.SelectedIndex = 0;
+
+            for (int x = 0; x <= 99; x++)
+            {
+                comboBoxRemainingTime.Items.Add(x);
+            }
+            comboBoxRemainingTime.SelectedIndex = 0;
+        }
+
+        string encodePassword(int num)
+        {
+            string result = "";
+
+            switch (num)
+            {
+                case 0:
+                    result = "A";
+                    break;
+                case 1:
+                    result = "B";
+                    break;
+                case 2:
+                    result = "C";
+                    break;
+                case 3:
+                    result = "D";
+                    break;
+                case 4:
+                    result = "E";
+                    break;
+                case 5:
+                    result = "F";
+                    break;
+                case 6:
+                    result = "G";
+                    break;
+                case 7:
+                    result = "H";
+                    break;
+                case 8:
+                    result = "I";
+                    break;
+                case 9:
+                    result = "J";
+                    break;
+                case 10:
+                    result = "K";
+                    break;
+                case 11:
+                    result = "L";
+                    break;
+                case 12:
+                    result = "M";
+                    break;
+                case 13:
+                    result = "N";
+                    break;
+                case 14:
+                    result = "O";
+                    break;
+                case 15:
+                    result = "P";
+                    break;
+                case 16:
+                    result = "Q";
+                    break;
+                case 17:
+                    result = "R";
+                    break;
+                case 18:
+                    result = "S";
+                    break;
+                case 19:
+                    result = "T";
+                    break;
+                case 20:
+                    result = "U";
+                    break;
+                case 21:
+                    result = "V";
+                    break;
+                case 22:
+                    result = "W";
+                    break;
+                case 23:
+                    result = "X";
+                    break;
+                case 24:
+                    result = "Y";
+                    break;
+                case 25:
+                    result = "Z";
+                    break;
+                default:
+                    result = "?";
+                    // Got a value less than 0 or greater than 25. Should not happen. Password generation Failed.
+                    break;
+            }
+            return result;
+        }
+
+        private void buttonGeneratePassword_Click(object sender, EventArgs e)
+        {
+            int level = (int)comboBoxLevel.SelectedItem;
+            int health = (int)comboBoxHealth.SelectedItem;
+            int remainingTime = (int)comboBoxRemainingTime.SelectedItem;
+
+            int digit1 = 0;
+            int digit2 = 0;
+            int digit3 = 0;
+            int digit4 = 0;
+            int digit5 = 0;
+            int digit6 = 0;
+
+            digit1 = level - 1;
+            digit3 = remainingTime % 10;
+            digit2 = (remainingTime - digit3) / 10;
+            digit4 = health;
+            digit5 = 0;
+            digit6 = (10 + digit1 + digit2 + digit3 + digit4 + digit5) % 26;
+
+            textBoxPassword.Text = encodePassword(digit1) + encodePassword(digit2) + encodePassword(digit3) + encodePassword(digit4) + encodePassword(digit5) + encodePassword(digit6);
+        }
+    }
+}
